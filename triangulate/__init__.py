f38572882ce9b7e5aac999c435e9626d8c25218e
Simone Cesano <simonecesano@libero.it>
2024-07-30 08:56:23 +0200

    Cleaned up, improved boundary check

--- triangulate/__init__.py
+++ triangulate/__init__.py
@@ -6,54 +6,14 @@ from shapely import Polygon, LinearRing, Point, MultiPoint, MultiPolygon, covera
 from shapely.ops import triangulate
 import bmesh
 
-def get_ordered_boundary_vertices_no_holes(obj):
-    bm = bmesh.new()
-    bm.from_mesh(obj.data)
-
-    bm.edges.ensure_lookup_table()
-    bm.verts.ensure_lookup_table()
-    bm.faces.ensure_lookup_table()
-
-    boundary_edges = [edge for edge in bm.edges if len(edge.link_faces) < 2]
-
-    if not boundary_edges:
-        bm.free()
-        return []
-    print("made it to here")
-    # Find an edge to start with
-    edge = boundary_edges[0]
-    vert = edge.verts[0]
-    
-    ordered_vertices = [vert]
-    current_edge = edge
-    boundary_edges.remove(current_edge)
-    
-    while boundary_edges:
-        next_vert = current_edge.other_vert(vert)
-        ordered_vertices.append(next_vert)
-        vert = next_vert
-        
-        # Find the next edge
-        for edge in boundary_edges:
-            if vert in edge.verts:
-                current_edge = edge
-                boundary_edges.remove(current_edge)
-                break
-
-    # Extract coordinates before freeing the BMesh
-    retval = [(v.co.x, v.co.y) for v in ordered_vertices]
-    bm.free()
-    return retval
-
-def obj_to_poly_no_holes(obj):
-    ordered_vertices = get_ordered_boundary_vertices(obj)
-    return Polygon(ordered_vertices)
-
 def get_ordered_boundary_edges(obj):
-    bm = bmesh.new()
-    bm.from_mesh(obj.data)
-    bm.edges.ensure_lookup_table()
-    bm.verts.ensure_lookup_table()
+    if isinstance(obj, bmesh.types.BMesh):
+        bm = obj.copy()
+    else:
+        bm = bmesh.new()
+        bm.from_mesh(obj.data)
+        bm.edges.ensure_lookup_table()
+        bm.verts.ensure_lookup_table()
 
     # Find boundary edges (edges with only one linked face)
     boundary_edges = [edge for edge in bm.edges if len(edge.link_faces) < 2]
@@ -112,9 +72,7 @@ def get_ordered_boundary_edges(obj):
 
 def obj_to_poly(obj):
     boundary_loops = get_ordered_boundary_edges(obj)
-
-    if not boundary_loops:
-        return None
+    if not boundary_loops: return None
 
     # Determine the outer boundary and potential holes
     # Assumption: The largest loop is the outer boundary, and others are holes
@@ -125,26 +83,26 @@ def obj_to_poly(obj):
     polygon = Polygon(outer_boundary, holes)
     return polygon
 
-
-def coverage_ratio(polygon):
-    return polygon.area / polygon.envelope.area
-
 def export_poly(poly):
     with open('test.svg', 'w') as f:
         f.write('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink"><g transform="scale(1000 1000)">')
         f.write(p.svg())
         f.write('</g></svg>')
 
-def triangulate_poly_and_points(poly, points, shape_buffer=0.0001):
+def triangulate_poly_and_points(poly, points, shape_buffer=0.001):
     pts = points
 
-    r = poly.buffer(shape_buffer)
+    minx, miny, maxx, maxy = poly.bounds
+    l = max([maxx - minx, maxy - miny])
+    print(l, shape_buffer * l)
+    
+    r = poly.buffer(shape_buffer * l)
+    
     for p in list(poly.exterior.coords): pts.append(p)
+    for p in poly.interiors: pts.extend(p.coords)
 
+    
     triangles = [ t for t in shapely.ops.triangulate(shapely.MultiPoint(pts)) ]
-
-    # triangles = [ t for t in triangles if poly.contains(t) ]
-    # triangles = [ t for t in triangles if (poly.intersection(t).area / t.area) > 0.97 ]
     triangles = [ t for t in triangles if r.contains(t) ]
     
     points = {}
